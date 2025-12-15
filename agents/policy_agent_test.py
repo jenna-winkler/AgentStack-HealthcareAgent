947cac3842de98a17d1108bc2d14bd33dca67efb
Sandi Besen <sandibesen@gmail.com>
2025-12-15 11:59:33 -0800

    Adding main orchestrator agent and reworking policy agent.
    Signed-off-by: Sandi Besen <sandibesen@gmail.com>

--- agents/policy_agent_test.py
+++
@@ -1,7 +0,0 @@
-# scratch_test_gemini_policy.py
-from policy_agent_logic_gemini import GeminiPolicyAgent
-
-
-agent = GeminiPolicyAgent()
-print(agent.answer_query("What does this plan cover for mental health visits?"))
-
